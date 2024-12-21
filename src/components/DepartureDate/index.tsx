b9b30b5791fe6380c057e11fa55bf91d288beb39
TheSilkFlower <smirnova100991@gmail.com>
2024-12-21 10:32:23 +0300

    Создала компонент DepartureDate

---
+++ src/components/DepartureDate/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './index.module.scss'
+
+interface DepartureDateProps {
+    date: string
+}
+
+export const DepartureDate: React.FC<DepartureDateProps> = ({ date }) => {
+    return (
+        <div>{date}</div>
+    )
+}
