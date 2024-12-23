7c483e65cd02606e72f56565ff6a8a3950fd3537
TheSilkFlower <smirnova100991@gmail.com>
2024-12-23 23:20:00 +0300

    Обернула App в Provider redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import store from './stores/store'
+import { Provider } from 'react-redux'
 import './App/index.module.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
