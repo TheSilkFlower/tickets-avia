0e0e876e05a959234ca623ebf1843f01c465b806
TheSilkFlower <smirnova100991@gmail.com>
2025-01-07 10:00:18 +0300

    Экспорт RootState, AppDispatch

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import currencyReducer from "../features/currency";
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
         currency: currencyReducer
     }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
