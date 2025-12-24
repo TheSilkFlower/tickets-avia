869daa790ca219bb58968a7007b7046d46b56b0c
TheSilkFlower <smirnova100991@gmail.com>
2025-12-24 19:46:21 +0300

    Добавила селектор для получения валюты в переменную currType

--- src/components/TicketCost/index.tsx
+++ src/components/TicketCost/index.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import styles from './index.module.scss';
+import { useAppSelector } from "../../hooks/state";
 
 interface TicketCostInfo {
     cost: number
 } 
 
 export const TicketCost: React.FC<TicketCostInfo> = ( { cost } ) => {
+    const curr = useAppSelector(state => state.currency.curr);
+    let currType;
+    switch(curr) {
+        case 'RUB':
+            currType = '₽';
+            break;
+        case 'EUR':
+            currType = '€';
+            break;
+        case 'USD':
+            currType = '$';
+            break;
+        default: currType = '₽';
+    }
     
     return (
         <div className={ styles.info }>
-            <button className={ styles.buttonBuy }>Купить за {cost}</button>
+            <button className={ styles.buttonBuy }>Купить за {cost} {currType}</button>
         </div>
     )
 }
